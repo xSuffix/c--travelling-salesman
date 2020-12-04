f85747a50a02d255eceeeac7a553ec19f0f85357
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2020-12-04 15:48:54 +0100

    Implement basic main menu input
    The program will now call functions based on the input (a-f) of the user.

--- main.c
+++ main.c
@@ -3,19 +3,44 @@
 
 #pragma execution_character_set("utf-8")
 
+void loadData() {
+  printf("load");
+}
+
+void saveData() {
+  printf("save");
+}
+
+void showData() {
+  printf("show");
+}
+
+void changeDistanceBetweenCities() {
+  printf("change distance");
+}
+
+void calculateShortestRoute() {
+  printf("calculate");
+}
+
+void exitProgram() {
+  printf("exit");
+}
+
 struct menu
 {
   char key;
   char *description;
+  void (*fun_ptr)();
 };
 
 struct menu startMenu[] = {
-    {'a', "Entfernungstabelle laden"},
-    {'b', "Entfernungstabelle speichern"},
-    {'c', "Entfernungstabelle anzeigen"},
-    {'d', "Entfernung zwischen zwei Städten ändern"},
-    {'e', "Kürzeste Route berechnen"},
-    {'f', "Programm beenden"},
+    {'a', "Entfernungstabelle laden", &loadData},
+    {'b', "Entfernungstabelle speichern", &saveData},
+    {'c', "Entfernungstabelle anzeigen", &showData},
+    {'d', "Entfernung zwischen zwei Städten ändern", &changeDistanceBetweenCities},
+    {'e', "Kürzeste Route berechnen", &calculateShortestRoute},
+    {'f', "Programm beenden", &exitProgram},
 };
 
 int main()
@@ -23,13 +48,20 @@ int main()
   SetConsoleOutputCP(65001); // utf-8
 
   int startMenuLength = sizeof(startMenu) / sizeof startMenu[0];
-  for (int i = 0; i < startMenuLength; i++) {
+  for (int i = 0; i < startMenuLength; i++)
+  {
     printf("(%c) %s\n", startMenu[i].key, startMenu[i].description);
   }
 
   char c;
   scanf("%c", &c);
+  printf("\n");
 
-  
+  for (int i = 0; i < startMenuLength; i++) {
+    if (c == startMenu[i].key) {
+      startMenu[i].fun_ptr();
+    }
+  }
+
   return 0;
 }
